bd236ba9e1327e77f2bbcde340aef12958d101d6
iHSAN <ihsanulrahman@proton.me>
2026-08-10 01:47:52 +0500

    repo_clone: update for a17
    Signed-off-by: iHSAN <ihsanulrahman@proton.me>

--- repo_clone.sh
+++ repo_clone.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 declare -a REPOS=(
-    "https://github.com/mi-atoll/device_xiaomi_miatoll|device/xiaomi/miatoll|16-volt|0"
-    "https://github.com/mi-atoll/vendor_xiaomi_miatoll|vendor/xiaomi/miatoll|16|1"
-    "https://github.com/mi-atoll/vendor_xiaomi_miuicamera-miatoll|vendor/xiaomi/miuicamera-miatoll|16|1"
-    "https://github.com/ihsanulrahman/android_kernel_xiaomi_sm6250|kernel/xiaomi/sm6250|16|1"
-    "https://github.com/LineageOS/android_hardware_sony_timekeep|hardware/sony/timekeep|lineage-23.0|0"
-    "https://github.com/ihsanulrahman/vendor_voltage-priv_keys|vendor/voltage-priv/keys|16|0"
+    "https://github.com/mi-atoll/device_xiaomi_miatoll|device/xiaomi/miatoll|17-volt|0"
+    "https://github.com/mi-atoll/vendor_xiaomi_miatoll|vendor/xiaomi/miatoll|17|1"
+    "https://github.com/mi-atoll/leica-device_xiaomi_miuicamera-miatoll|device/xiaomi/miuicamera-miatoll|17|1"
+    "https://github.com/mi-atoll/leica-vendor_xiaomi_miuicamera-miatoll|vendor/xiaomi/miuicamera-miatoll|17|1"
+    "https://github.com/ihsanulrahman/android_kernel_xiaomi_sm6250|kernel/xiaomi/sm6250|17|1"
+    "https://github.com/LineageOS/android_hardware_sony_timekeep|hardware/sony/timekeep|lineage-24.0|0"
+    "https://github.com/ihsanulrahman/vendor_voltage-priv_keys|vendor/voltage-priv/keys|17|0"
 )
 
 G='\033[0;32m'; R='\033[0;31m'; Y='\033[1;33m'; C='\033[0;36m'; E='\033[0m'
